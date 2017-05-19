f89b0acef7f2ad6adf8e3e1d65e869b2d57cf0ee
Efosa Oyegun <efosao@gmail.com>
2017-05-18 21:30:23 -0700

    added mock Ajax request

--- js/actions/user.js
+++ js/actions/user.js
@@ -1,11 +1,17 @@
+const axios = require('axios');
+
 
 import type { Action } from './types';
 
 export const SET_USER = 'SET_USER';
 
 export function setUser(user:string):Action {
-  return {
-    type: SET_USER,
-    payload: user,
-  };
+  return axios('https://js2go.com/api/package/express')
+    .then(result => {
+      return {
+        type: SET_USER,
+        payload: user,
+        data: result,
+      };
+    })
 }
